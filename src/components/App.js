51815e02dd801f780679ef211426f52c19ae6fb2
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-18 07:44:57 -0400

    set information flow

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import FoodForm from './FoodForm';
+import FoodCard from './FoodCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <FoodForm />
+      <FoodCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header () => {
+  return (
+    <header>
+        <h1>HUNGER SMASH</h1>
+        <img src="./images/hungerSmashLogo" alt="pic of empty plate" />
+      </header>
+  )
+}
+export default Header;
