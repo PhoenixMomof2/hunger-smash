d981fc97e43df06f67dc0eaa0ac45495bd27b7d4
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-24 16:41:50 -0400

    create style.css

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,18 @@ import React from 'react';
 import Header from './Header';
 import FoodForm from './FoodForm';
 import FoodCard from './FoodCard';
+import { useState } from 'react';
+
+const url = 'http://localhost:3001/foods'
 
 function App() {
+
+  useState(() => {
+    fetch(url)
+    .then(res=>res.json())
+    .then(foods=> console.log(foods))
+  },[])
+  
   return (
     <div className="App">
       <Header />
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function FoodCard() {
-  return <div>FoodCard</div>;
+  return (
+  <div className="food-Container">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+    </table>
+    </div>
+    )
 }
 
 export default FoodCard;
