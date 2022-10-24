30f6147b89bbce0ec11c9beb6df84430ce4837dd
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-24 18:10:40 -0400

    create nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 import Header from './Header';
+import NavBar from './NavBar';
 import FoodForm from './FoodForm';
 import FoodCard from './FoodCard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const url = 'http://localhost:3001/foods'
 
 function App() {
 
-  useState(() => {
+  useEffect(() => {
     fetch(url)
     .then(res=>res.json())
     .then(foods=> console.log(foods))
   },[])
-  
+
+  //const [meal, setMeal] = useState("")
+
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <FoodForm />
       <FoodCard />
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <button className="about">About</button>
+        <button className="taste-category">Taste Category</button>
+        <button className="hunger-smash">Smash Hunger</button> 
+      </nav>
+    </div>
+  )
+}
+
+
