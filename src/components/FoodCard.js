50c38ce6b17989aaf9cd42d687575707b98ec0ff
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-27 15:45:31 -0400

    format nav bar

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -4,10 +4,10 @@ function FoodCard({foods}) {
   return (
     <>
       <li className="cards_item">
+      <div className="card_title">{foods.name}</div>
         <div className="card">
           <img className="card_image" src={foods.imgUrl} alt={foods.name} />
           <div className="card_content">
-            <div className="card_title">{foods.name}</div>
             <p className="card_text">{foods.category}</p>
             <div className="card__detail">
               <p>{foods.description}</p>
