6066202fa339f5ca5267baabe229ba0e5935454f
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-02 17:33:17 -0400

    add react router

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+Client Side Routing
+-URLs
+- REST REpresentation State Transfer
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./style.css";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
-
