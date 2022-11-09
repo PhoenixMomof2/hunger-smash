fd2ccb5ed15e5dda4ef93947a915ea11f8babb0b
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-09 15:43:18 -0500

    refactor code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
 import About from "./About";
 import FoodList from "./FoodList";
 import FoodForm from "./FoodForm";
 
 const App = () => {
-
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
@@ -15,31 +14,29 @@ const App = () => {
     fetch("http://localhost:3001/foods")
       .then((res) => res.json())
       .then((foods) => setFoods(foods));
-      
   }, []);
 
-function handleAddNewFood(newFood){
-  setFoods([...foods, newFood]) // Updating foods state.
- }
+  function handleAddNewFood(newFood) {
+    setFoods([...foods, newFood]); // Updating foods state.
+  }
   return (
     <div>
       <Header />
-      <br/>
+      <br />
       <NavBar />
       <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/foods">
-            <FoodList foods={foods}/>
-          </Route>
-          <Route exact path="/newFoods">
-            <FoodForm handleAddNewFood={handleAddNewFood}/>
-          </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/foods">
+          <FoodList foods={foods} />
+        </Route>
+        <Route exact path="/newFoods">
+          <FoodForm handleAddNewFood={handleAddNewFood} />
+        </Route>
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
-
